23c49c187f7d3aeb045d4bb92e5a9bb3c4c8168f
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-08-23 00:04:50 -0400

    Small changes

--- recepie-center/src/RecipeMini.jsx
+++ recepie-center/src/RecipeMini.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 // eslint-disable-next-line react/prop-types
 const RecipeMini = ({name, image, ingredients, calories}) => {
     return (
-        <div className="RecipeMini">
+        <Link to={name} className="RecipeMini">
             <img className="recipe-mini-image" src={image}></img>
             <div className="recipe-mini-description">
                 <h1 className="recipe-mini-name">{name}</h1>
                 <p className="recipe-mini-ingredients">Ingredients: {ingredients}</p>
                 <p className="recipe-mini-calories">{calories} calories</p>
             </div>
-        </div>
+        </Link>
     )
 }
 
